2a2ef68bedcf6dab70fbbfe0e57cf101f3d3f898
yuragasyuk <yura.gasyuk@gmail.com>
2017-11-13 18:57:45 +0300

    Using GET

--- headlines.py
+++ headlines.py
@@ -2,6 +2,8 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -13,11 +15,15 @@ RSS_FEEDS = {'habr': 'https://habrahabr.ru/rss/hubs/all/',
 
 # We can have more than one decorator per function.
 @app.route("/")
-@app.route("/<publication>")
-# We keep our default value for the publication parameter.
-def get_news(publication="habr"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "habr"
+    else:
+        publication = query.lower()
     # Parse the feed. Function will download the feed, parses it and returns a Python dictionary.
     feed = feedparser.parse(RSS_FEEDS[publication])
+    # Rendering our template with parameters (send data to view).
     return render_template("home.html", articles=feed['entries'], count=len(feed['entries']))
 
 
